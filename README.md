3473d49f27320ea6aad30ece444cb2b3d305f1f0
pl4netary <kroll.logan@gmail.com>
2024-04-19 13:29:31 -0400

    Start server creation

---
+++ README.md
@@ -0,0 +1,10 @@
+# [PRE-ALPHA] FASINO
+Fasino is the perfect game for all you gambling addicts out there! It serves a way to get that gambing high everyone wants without completely obliterating your retirement savings!
+
+## About Development
+
+Fasino is currently in the development stages, meaning that it won't be available for a while.
+
+We accept pull requests! We're using no external code (only using node.js to run the server, not for NPM), and we comment our code so those who wish to contribute can.
+
+TLDR: Fasino is currently in development. Come back later for updates!
---
+++ src/index.js
@@ -0,0 +1,36 @@
+// Import utilities
+const colors = require('./utils/colors.js');
+
+// Import the HTTP and FS modules from Node.JS for server creation
+const http = require('http');
+const fs = require('fs');
+
+// Add server constants
+const PORT = process.env.PORT;
+const HOST = process.env.HOST;
+
+// Create a serve function to serve a given file to a given path
+const serve = (res, path, contentType) => {
+    fs.readFile(path, (err, file) => {
+        if (err) console.log(err);
+        res.writeHead(200, { 'Content-Type': contentType });
+        res.end(file);
+    });
+}
+
+// Create a server object
+const SERVER = http.createServer((req, res) => {
+    
+    // Server logic
+    switch (req.url) {
+        case '/':
+            serve(res, './src/html/home.html', 'text/html');
+            break;
+    }
+
+});
+
+// Start the server
+SERVER.listen(PORT, HOST, () => {
+    console.log(`${colors.FgYellow}[SERVER]: Server is up on http://${HOST}:${PORT}.${colors.Reset}`);
+});
